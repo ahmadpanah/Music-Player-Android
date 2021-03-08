3b4256df5aa23ea9d274ff87d95880ab6ad50957
Hossein <djalicrt@gmail.com>
2021-03-08 17:12:50 +0330

    Finish Music Player Project

---
+++ app/src/main/java/ir/shariaty/musicplayer/MainActivity.java
@@ -0,0 +1,213 @@
+package ir.shariaty.musicplayer;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView play, prev, next, coversong;
+    TextView songTitle;
+    SeekBar mSeekBarTime, mSeekBarVol;
+    static MediaPlayer mMediaPlayer;
+    private Runnable runnable;
+    private AudioManager mAudioManager;
+    int currentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        // init Views
+        play = findViewById(R.id.play);
+        prev = findViewById(R.id.prev);
+        next = findViewById(R.id.next);
+        songTitle = findViewById(R.id.songTitle);
+        coversong = findViewById(R.id.coversong);
+        mSeekBarTime = findViewById(R.id.seekBarTime);
+        mSeekBarVol = findViewById(R.id.seekBarVol);
+
+        // Creating an ArrayList to store our Songs.
+
+        final ArrayList<Integer> songs = new ArrayList<>();
+        songs.add(0, R.raw.neghab);
+        songs.add(1, R.raw.pashimoonam);
+        songs.add(2, R.raw.chibayadmigoftam);
+
+        // init Media Player
+
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+
+        // Seekbar Volume
+
+        int maxV = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curV = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mSeekBarVol.setMax(maxV);
+        mSeekBarVol.setProgress(curV);
+
+        mSeekBarVol.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mSeekBarTime.setMax(mMediaPlayer.getDuration());
+                if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.pause();
+                    play.setImageResource(R.drawable.ic_play);
+                } else {
+                    mMediaPlayer.start();
+                    play.setImageResource(R.drawable.ic_pause);
+                }
+
+                songNames();
+
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayer != null) {
+                    play.setImageResource(R.drawable.ic_pause);
+                }
+
+                if (currentIndex < songs.size() - 1) {
+                    currentIndex++;
+                } else {
+                    currentIndex = 0;
+                }
+
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                }
+
+                mMediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+                mMediaPlayer.start();
+                songNames();
+            }
+        });
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayer != null) {
+                    play.setImageResource(R.drawable.ic_pause);
+                }
+
+                if (currentIndex > 0) {
+                    currentIndex--;
+                } else {
+                    currentIndex = songs.size() - 1;
+                }
+
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                }
+
+                mMediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+                mMediaPlayer.start();
+                songNames();
+            }
+        });
+
+    }
+
+    private void songNames() {
+        if (currentIndex == 0) {
+            songTitle.setText("Ali Yasini - Neghab");
+            coversong.setImageResource(R.drawable.neghab);
+        }
+        if (currentIndex == 1) {
+            songTitle.setText("Naser PourKaram - Pashimoonam");
+            coversong.setImageResource(R.drawable.pashimoonam);
+        }
+        if (currentIndex == 2) {
+            songTitle.setText("Arman Esmaeili - Chi Bayad Migoftam");
+            coversong.setImageResource(R.drawable.chibayadmigoftam);
+        }
+
+        // SeekBar Duration
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mSeekBarTime.setMax(mMediaPlayer.getDuration());
+                mMediaPlayer.start();
+            }
+        });
+
+        mSeekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mMediaPlayer.seekTo(progress);
+                    mSeekBarTime.setProgress(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mMediaPlayer != null) {
+                    try {
+                        if (mMediaPlayer.isPlaying()) {
+                            Message message = new Message();
+                            message.what = mMediaPlayer.getCurrentPosition();
+                            handler.sendMessage(message);
+                            Thread.sleep(1000);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @SuppressLint("HandlerLeak") Handler handler = new Handler() {
+        @Override
+        public void handleMessage (Message msg) {
+            mSeekBarTime.setProgress(msg.what);
+        }
+    };
+}
